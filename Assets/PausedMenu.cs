c3ecc86728db62716982ea041982fec8396fc906
Pinak Jalan <pinak.jalan@me.com>
2017-02-19 18:22:24 -0700

    Fixed pause and Start menus

--- Assets/PausedMenu.cs
+++ Assets/PausedMenu.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PausedMenu : MonoBehaviour {
-	public Button PauseButton;
 	public GameObject PauseMenu;
 	// Use this for initialization
 	void Start () {
-		PauseButton.enabled = true;
 		PauseMenu.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+			Time.timeScale =0;
+			PauseMenu.SetActive (true);
+			
+		}
 		
 	}
 
 	public void OnPauseButtonClicked()
 	{
-		Time.timeScale =0;
+		//Only need this code if we're not mapping pause to a button
+		/*Time.timeScale =0;
 		PauseButton.enabled = false;
-		PauseMenu.SetActive (true);
+		PauseMenu.SetActive (true);*/ 
 	}
 	public void OnRestartButtonClicked()
 	{
@@ -29,12 +35,17 @@ public class PausedMenu : MonoBehaviour {
 	}
 	public void OnResumeButtonClicked()
 	{
+		//Cursor.visible = false;
+		//Cursor.lockState = CursorLockMode.None;
 		Time.timeScale =1;
-		PauseButton.enabled = true;
 		PauseMenu.SetActive (false);
 	}
 	public void OnExitButtonClicked()
 	{
 		Application.Quit ();
+	}
+	public void OnMainMenuExitClicked()
+	{
+		Application.LoadLevel ("PinakScene");
 	}
 }
--- Assets/_Scripts/StartMenu.cs
+++ Assets/_Scripts/StartMenu.cs
@@ -11,6 +11,8 @@ public class StartMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
 		
 	}
 
