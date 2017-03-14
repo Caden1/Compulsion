198ae4bb7880400434dd8a3d57a5d930a16f71e6
Cameron Minkel <kamaros173@gmail.com>
2017-03-13 23:41:40 -0600

    Worked  on scripts for activatables

---
+++ Assets/_Scripts/FridgeDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeDoor : MonoBehaviour {
+
+    public float speed = 1f;
+
+    private bool isOpen;
+    private Quaternion closed;
+    private Quaternion open;
+    private Quaternion target;
+
+	// Use this for initialization
+	private void Start () {
+        isOpen = false;
+        closed = transform.rotation;
+        open = Quaternion.AngleAxis(-90f, Vector3.up) *  transform.rotation;
+        target = closed;
+	}
+	
+    private void Update()
+    {
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * speed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target, Time.deltaTime * speed);
+    }
+
+	public void Activate()
+    {
+
+        if (isOpen)
+        {
+            target = closed;
+        }
+        else
+        {
+            target = open;
+        }
+
+        isOpen = !isOpen;
+    }
+}
---
+++ Assets/_Scripts/OvenDoor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvenDoor : MonoBehaviour {
+
+    public float speed = 60f;
+
+    private bool isOpen;
+    private Quaternion closed;
+    private Quaternion open;
+    private Quaternion target;
+
+    // Use this for initialization
+    private void Start()
+    {
+        isOpen = false;
+        closed = transform.rotation;
+        open = Quaternion.AngleAxis(-90f, Vector3.right) * transform.rotation;
+        target = closed;
+    }
+
+    private void Update()
+    {
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * speed);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target, Time.deltaTime * speed);
+    }
+
+    public void Activate()
+    {
+
+        if (isOpen)
+        {
+            target = closed;
+        }
+        else
+        {
+            target = open;
+        }
+
+        isOpen = !isOpen;
+    }
+}
