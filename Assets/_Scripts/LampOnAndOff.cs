b67f8ed69639d8d5db07df40b5d2b30ee49b2687
Caden <xbartonc@gmail.com>
2017-03-25 17:33:25 -0600

    Added code for turning lamp on and off.

--- Assets/_Scripts/LampOnAndOff.cs
+++ Assets/_Scripts/LampOnAndOff.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TVOnAndOff : MonoBehaviour
+public class LampOnAndOff : MonoBehaviour
 {
     public int numberOfTimesNeeded = 3; // Number needed to stop OCD
 
     private static int timesTurnedOnAndOff;
     private static bool isOn;
 
+    private GameObject lampString;
+
     // Use this for initialization
-	void Start ()
+    void Start()
     {
         timesTurnedOnAndOff = 0;
 
         isOn = false;
+
+        lampString = gameObject.transform.GetChild(2).gameObject;
     }
+
 
     public void Activate()
     {
         if (isOn == false)
         {
-            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(3).gameObject.SetActive(true);
             timesTurnedOnAndOff++;
             isOn = true;
         }
         else
         {
-            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(3).gameObject.SetActive(false);
             timesTurnedOnAndOff++;
             isOn = false;
         }
@@ -35,7 +40,7 @@ public class TVOnAndOff : MonoBehaviour
         if (timesTurnedOnAndOff == (numberOfTimesNeeded * 2))
         {
             timesTurnedOnAndOff = 0;
-            //Debug.Log("Complete");
+            Debug.Log("Complete");
 
             // STOP OCD EFFECTS HERE (SEND MESSAGE BACK TO CAMERON TO STOP IT??)
         }
