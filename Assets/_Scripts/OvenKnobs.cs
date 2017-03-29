47d143c6f02d893975dabdbda3cebe2c4a211f1b
Cadoni1 <xbartonc@gmail.com>
2017-03-28 21:10:59 -0600

    Got the oven knobs communicating with GameManager.

--- Assets/_Scripts/OvenKnobs.cs
+++ Assets/_Scripts/OvenKnobs.cs
@@ -5,32 +5,28 @@ using UnityEngine.UI;
 
 public class OvenKnobs : MonoBehaviour
 {
-
     private static Dictionary<string, bool> knobSet = new Dictionary<string, bool>();
+	private StartOvenKnobOCD startOvenKnobOCDScript;
 
     public float speed = 200f;
 	public static bool isStoveChecked = false;
-	public SinkKnob SK;
 
     private bool isOn;
-    private int knobCount = 4;
     private Quaternion on;
     private Quaternion off;
     private Quaternion target;
     private Coroutine cor;
-    private OCDEffectManager blur;
 
 
     // Use this for initialization
-    public void Start ()
+    public void Start()
     {
-		GameObject g = GameObject.FindGameObjectWithTag ("Activate");
-		SK = g.GetComponent<SinkKnob> ();
         isOn = false;
         off = transform.rotation;
         on = Quaternion.AngleAxis(-135f, transform.up) * transform.rotation;
         target = off;
-        blur = GameObject.Find("OCDEffectManager").GetComponent<OCDEffectManager>();
+
+		startOvenKnobOCDScript = GameObject.Find("StartOvenKnobOCD").GetComponent<StartOvenKnobOCD>();
     }
 
     public void Activate()
@@ -46,30 +42,32 @@ public class OvenKnobs : MonoBehaviour
         cor = StartCoroutine("Swing");
         isOn = !isOn;
 
-        if (knobSet.ContainsKey(gameObject.name))
-            knobSet[gameObject.name] = !knobSet[gameObject.name];
-        else
-            knobSet.Add(gameObject.name, true);
-
-        if (knobSet.Count == knobCount)
-        {
-            bool complete = true;
-            foreach (bool b in knobSet.Values)
-            {
-                if (b == true)
-                    complete = false;
-            }
-
-            if (complete)
-            {
-				isStoveChecked = true;
-                knobSet.Clear(); // Clear the knob set so it doesn't continue to execute this statement
-				GameObject.FindGameObjectWithTag ("OCDKnobs").GetComponent<Text> ().enabled = false;
-				// Call StopOCDTimer from the OCDEffectManager script to stop the OCD effects.
-				blur.StopOCDTimer ();
+		if (startOvenKnobOCDScript.isActivated == true) 
+		{
+			if (knobSet.ContainsKey (gameObject.name))
+				knobSet [gameObject.name] = !knobSet [gameObject.name];
+			else
+				knobSet.Add (gameObject.name, isOn);
+			
+			if (knobSet.Count == 4) 
+			{
+				bool complete = true;
+				foreach (bool b in knobSet.Values) 
+				{
+					if (b == true)
+						complete = false;
 				}
-            }
-        }
+
+				if (complete) 
+				{
+					isStoveChecked = true;
+					knobSet.Clear (); // Clear the knob set so it doesn't continue to execute this statement
+
+					startOvenKnobOCDScript.CleanUp();
+				}
+			}
+		}
+    }
     
 
     private IEnumerator Swing()
---
+++ Assets/_Scripts/StartOvenKnobOCD.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartOvenKnobOCD : MonoBehaviour 
+{
+	[HideInInspector]
+	public bool isActivated; // Set to true when the OCD task is activated.
+
+	public OvenKnobs ovenKnobsScript;
+
+	private GameObject gameManagerObject;
+
+	private void Start()
+	{
+		isActivated = false;
+
+		gameManagerObject = GameObject.Find("GameManager");
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			if (isActivated == false)
+			{
+				isActivated = true; // To prevent the OCD task playing everytime I exit the collider.
+
+				gameManagerObject.SendMessage("StartOCD", gameObject);
+
+				StartCoroutine(OCDActiveLength());
+			}
+		}
+	}
+
+	public void CleanUp()
+	{
+		gameManagerObject.SendMessage("EndOCD", gameObject);
+
+		StopCoroutine(OCDActiveLength());
+
+		StartCoroutine(ResetTimer());
+	}
+
+	/// <summary>
+	/// Allows this OCD effect to be triggered again.
+	/// </summary>
+	private IEnumerator ResetTimer()
+	{
+		yield return new WaitForSeconds(60f);
+
+		isActivated = false;
+	}
+
+	private IEnumerator OCDActiveLength()
+	{
+		while (true) 
+		{
+			yield return new WaitForSeconds (10);
+
+			// Make the text above this one a decent size (knobs are a big one).
+			// Start another coroutine here that calls an IEnumerable function that makes the 3D text pulse (Use a place-holder for now).
+			// After this ten seconds, make it pulse faster. After 5 times running through, cap it.
+
+			gameManagerObject.SendMessage("IncreaseInfluence", gameObject);
+		}
+	}
+}
