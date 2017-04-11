9706a52651ab2c5fe303a4bb4cd3e3ab9cc30902
Pinak Jalan <pinak.jalan@me.com>
2017-04-11 12:09:51 -0600

    Shower task done

---
+++ Assets/_Scripts/ShowerTask.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowerTask : MonoBehaviour {
+	private ParticleSystem water;
+	private GameManager gameManagerScript;
+
+
+	// Use this for initialization
+	void Start () {
+		gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
+		water = GameObject.Find("WaterParticleEffectShower").GetComponent<ParticleSystem>();
+		water.Stop();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Debug.Log (other.gameObject.name);
+		if (other.gameObject.name == "Player") {
+			water.Play ();
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		water.Stop ();
+		gameManagerScript.NormalTaskCompleted ();
+		gameObject.GetComponent<BoxCollider> ().enabled = false;
+
+	}
+}
---
+++ Assets/_Scripts/StickyNoteGoShower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyNoteGoShower : MonoBehaviour {
+
+	private GameObject ShowerArea;
+
+	// Use this for initialization
+	void Start () {
+		ShowerArea = GameObject.Find ("ShowerArea");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Activate()
+	{
+		// ADD SOUND EFFECT FOR RIPPING OFF STICKY NOTE
+		// Disable sticky note, but do not destroy it. It's needed by the GameManager.
+		gameObject.GetComponent<MeshRenderer>().enabled = false;
+		gameObject.GetComponent<BoxCollider>().enabled = false;
+		ShowerArea.GetComponent<BoxCollider> ().enabled = true;
+	}
+}
