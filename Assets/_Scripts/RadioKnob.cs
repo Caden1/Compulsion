f77e1f343683b9819bf0b490b1386eef19efc19a
Cameron Minkel <kamaros173@gmail.com>
2017-04-19 14:06:11 -0600

    Radio Prefab

---
+++ Assets/_Scripts/RadioKnob.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioKnob : MonoBehaviour {
+
+    public float speed = 30f;
+    public RadioPower radio;
+
+    private Quaternion old;
+    private Quaternion target;
+    private Coroutine cor;
+
+    // Use this for initialization
+    public void Start()
+    {
+        target = Quaternion.AngleAxis(-30f, Vector3.forward) * transform.rotation;
+    }
+
+    public void Activate()
+    {
+        if (cor != null)
+            StopCoroutine(cor);
+
+        radio.ChangeStation();
+        target = Quaternion.AngleAxis(-30f, Vector3.forward) * target; ;
+        cor = StartCoroutine("Swing");
+    }
+
+    public IEnumerator Swing()
+    {
+        while (Quaternion.Angle(transform.rotation, target) != 0)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target, Time.deltaTime * speed);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/_Scripts/RadioPower.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioPower : MonoBehaviour
+{
+
+    public float speed = 1f;
+    private enum FaceDirection { POSX, NEGX, POSZ, NEGZ }
+    [SerializeField] private FaceDirection facing = FaceDirection.POSX;
+    public AudioClip[] music;
+
+    private bool isOn;
+    private Vector3 off;
+    private Vector3 on;
+    private Vector3 target;
+    private Coroutine cor;
+    private Coroutine cor2;
+    private GameObject gameManager;
+    private AudioSource speaker;
+    private int clipNumber;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        isOn = false;
+        off = transform.position;
+        switch (facing)
+        {
+            case FaceDirection.POSX:
+                on = transform.position + (Vector3.left * 0.015f);
+                break;
+            case FaceDirection.NEGX:
+                on = transform.position + (Vector3.right * 0.015f);
+                break;
+            case FaceDirection.POSZ:
+                on = transform.position + (Vector3.back * 0.015f);
+                break;
+            case FaceDirection.NEGZ:
+                on = transform.position + (Vector3.forward * 0.015f);
+                break;
+        }
+            
+        gameManager = GameObject.Find("GameManager");
+        speaker = GetComponent<AudioSource>();
+        speaker.volume = 0;
+        clipNumber = 0;
+        speaker.clip = music[clipNumber++];
+        speaker.Play();
+    }
+
+    public void Activate()
+    {
+        if (cor != null)
+            StopCoroutine(cor);
+
+        if (isOn)
+        {
+            speaker.volume = 0;
+            transform.position = off;
+        }
+        else
+        {
+            speaker.volume = 1;
+            transform.position = on;
+        }
+
+        isOn = !isOn;
+    }
+
+    public bool isTurnedOn()
+    {
+        return isOn;
+    }
+
+    public void ChangeStation()
+    {
+        if (isOn)
+        {
+            if (clipNumber >= music.Length)
+            {
+                clipNumber = 0;
+            }
+
+            speaker.Stop();
+            speaker.clip = speaker.clip = music[clipNumber++];
+            Invoke("speaker.Play()", 0.1f);
+        }
+
+        
+    }
+}
